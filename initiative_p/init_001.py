346457f59b2a5db0e76e0c0a355d055cea41705e
Fernando M. Oliveira <koynonia@gmail.com>
2019-11-04 14:53:33 -0300

    Inciando os estudos em Python.

---
+++ initiative_p/init_001.py
@@ -0,0 +1,30 @@
+﻿import sys
+from math import cos, radians
+import numpy as np     # installed with matplotlib
+import matplotlib.pyplot as plt
+
+#print("Hello, Visual Studio")
+
+#for i in range(360):
+#    print(cos(radians(i)))
+
+# Create a string with spaces proportional to a cosine of x in degrees
+def make_dot_string(x):
+    rad = radians(x)                             # cos works with radians
+    numspaces = int(20 * cos(radians(x)) + 20)   # scale to 0-40 spaces
+    st = ' ' * numspaces + 'o'                   # place 'o' after the spaces
+    return st
+
+#def main():
+#    for i in range(0, 1800, 12):
+#        s = make_dot_string(i)
+#        print(s)
+
+#main()
+
+def main():
+    x = np.arange(0, radians(1800), radians(12))
+    plt.plot(x, np.cos(x), 'b')
+    plt.show()
+
+main()
