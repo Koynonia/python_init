d3bfb5af1cad2b37e7ace8a89e45540afbbefa64
Fernando M. Oliveira <koynonia@gmail.com>
2019-11-04 17:46:15 -0300

    Incremento dos testes.

--- initiative_p/init_001.py
+++ initiative_p/init_001.py
@@ -3,15 +3,29 @@ from math import cos, radians
 import numpy as np     # installed with matplotlib
 import matplotlib.pyplot as plt
 
-#print("Hello, Visual Studio")
+# Imprime Hello e pergunta o meu nome.
+
+print("Hello, Visual Studio")
+print()
+print('Qual o seu nome?') # pergunta o nome
+myName = input()
+print()
+print('Como é bom conhecer você, ' + myName)
+print('O tamanho do seu nome em caracteres é: ' + str(len(myName)))
+print()
+print('Qual a sua idade?') # pergunta a idade
+myAge = input()
+print()
+print('Você irá fazer ' + str(int(myAge) + 1) + ' em um ano.')
+
+# Create a string with spaces proportional to a cosine of x in degrees
 
 #for i in range(360):
 #    print(cos(radians(i)))
 
-# Create a string with spaces proportional to a cosine of x in degrees
 def make_dot_string(x):
     rad = radians(x)                             # cos works with radians
-    numspaces = int(20 * cos(radians(x)) + 20)   # scale to 0-40 spaces
+    numspaces = int(20 * cos(rad) + 20)          # scale to 0-40 spaces
     st = ' ' * numspaces + 'o'                   # place 'o' after the spaces
     return st
 
